d6052c22f0392081b226cfafd5e8c8d288fce9ee
Ishika Patel <ishikapatel1364@gmail.com>
2025-01-30 21:56:19 +0530

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,21 @@ const App = () => {
 
   return (
     <>
-      <div className='flex h-screen w-screen bg-purple-300 justify-center items-center'>
+      <div className='flex h-screen w-screen bg-purple-300 justify-center items-center rounded-lg'>
         <div className='flex h-[70%] w-[60%] bg-white rounded-lg flex-col'>
-          <div className='flex h-[15%] w-[100%] bg-slate-700 justify-center items-center'>
+          <div className='flex h-[15%] w-[100%]  justify-center items-center'>
             <h1 className=' text-5xl m-2'>TODO</h1>
           </div>
-          <div className='flex h-[15%] w-[100%] bg-slate-500 items-center justify-center '>
+          <div className='flex h-[15%] w-[100%]  items-center justify-center '>
             <input
               type='text'
               value={todo}
-              className='flex border-2 h-[50%] w-[58%] rounded-md '
+              className='flex border-2 h-[50%] w-[70%] rounded-md '
               onChange={inputHandler}
             />
+            <button className='border-2 py-2 px-6 rounded-md bg-white ml-2'>
+              Add
+            </button>
           </div>
         </div>
       </div>
