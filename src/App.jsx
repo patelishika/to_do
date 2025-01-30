239180b0d3287ae233b50dd506aecf06e3aa68b1
Ishika Patel <ishikapatel1364@gmail.com>
2025-01-30 23:02:55 +0530

    add todo functionality

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
     setTodo(event.target.value);
   };
 
+  const AddTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo('');
+  };
+
   return (
     <>
       <div className='flex h-screen w-screen bg-purple-300 justify-center items-center rounded-lg'>
@@ -22,9 +27,24 @@ const App = () => {
               className='flex border-2 h-[50%] w-[70%] rounded-md '
               onChange={inputHandler}
             />
-            <button className='border-2 py-2 px-6 rounded-md bg-white ml-2'>
+            <button
+              className='border-2 py-2 px-6 rounded-md bg-white ml-2'
+              onClick={AddTodo}>
               Add
             </button>
+          </div>
+          <div className='flex h-[70%] w-[100%]  flex-col'>
+            {todos.map((t) => {
+              return (
+                <>
+                  <div className='flex h-[10%] w-[100%]  justify-around items-center mt-1'>
+                    <div className='flex h-[100%] w-[85%] border-2 bg-white border-black rounded-md'>
+                      <h1 className='text-2xl'>{t}</h1>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
