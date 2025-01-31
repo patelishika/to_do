469a12af00dc7f75eac2390f9af62b95af6d3752
Ishika Patel <ishikapatel1364@gmail.com>
2025-02-01 00:07:20 +0530

    edit todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,27 @@ import { useState } from 'react';
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState('');
+  const [editTodo, setEditTodo] = useState(null);
 
   const inputHandler = (event) => {
     setTodo(event.target.value);
   };
 
   const AddTodo = () => {
-    setTodos([...todos, { id: Date.now(), text: todo }]);
-    setTodo('');
-    if (todo == '') {
+    if (todo.trim() === '') {
       alert('please enter the todo');
+      return;
     }
+
+    if (editTodo) {
+      setTodos(
+        todos.map((t) => (t.id === editTodo ? { ...todos, text: todo } : todos))
+      );
+      setEditTodo(null);
+    } else {
+      setTodos([...todos, { id: Date.now(), text: todo }]);
+    }
+    setTodo('');
   };
 
   const deleteTodo = (t) => {
@@ -24,6 +34,11 @@ const App = () => {
         }
       })
     );
+  };
+
+  const editHandler = (t) => {
+    setTodo(t.text);
+    setEditTodo(t.id);
   };
 
   return (
@@ -38,12 +53,13 @@ const App = () => {
               type='text'
               value={todo}
               className='flex border-2 h-[50%] w-[70%] rounded-md '
+              placeholder='add todo'
               onChange={inputHandler}
             />
             <button
               className='border-2 py-2 px-6 rounded-md border-gray-300 ml-2'
               onClick={AddTodo}>
-              Add
+              {editTodo ? 'Update' : 'Add'}
             </button>
           </div>
           <div className='flex h-[70%] w-[100%]  flex-col justify-start items-center'>
@@ -53,13 +69,20 @@ const App = () => {
                   key={t.id}
                   className=' p-5 mt-3 flex h-[10%] w-[85%] border-2 border-gray-300 rounded-md justify-between items-center'>
                   <h1 className='text-2xl'>{t.text}</h1>
-                  <div className='flex h-[85%] w-[15%] justify-end items-center '>
+                  <div className='flex h-[85%] w-[12%] justify-around items-center '>
                     <button
                       className='flex border-2 border-gray-300 h-5 w-5 p-4 text-center rounded-full justify-center items-center'
                       onClick={() => {
                         deleteTodo(t);
                       }}>
                       x
+                    </button>
+                    <button
+                      className='flex border-2 border-gray-300 h-5 w-5 p-4 text-center rounded-full justify-center items-center'
+                      onClick={() => {
+                        editHandler(t);
+                      }}>
+                      +
                     </button>
                   </div>
                 </div>
